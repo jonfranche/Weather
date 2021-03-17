6dcce819ff6b21cf39b42e3e3a909dbab9ab8b37
jonfranche <jonfranche@gmail.com>
2021-03-17 15:07:20 -0700

    added text box for user to submit location

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 
 const apiKey = "66d4b6288d87d0f8fe671b63d5ffa59d";
+const userLocation = document.getElementById("location");
+const submitBttn = document.getElementById("submit");
 
-function getWeather(location) {
+async function getWeather(location) {
     fetch(`https:api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`, {mode: 'cors'})
         .then(function(response) {
             return response.json();
@@ -11,3 +13,6 @@ function getWeather(location) {
         });
 }
 
+submitBttn.addEventListener('click', function() {
+    getWeather(`${userLocation.value}`);
+});
